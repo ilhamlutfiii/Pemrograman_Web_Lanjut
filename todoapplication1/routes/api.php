e1ea334284c8b561043f7018781aa59701f18458
= <ilhamkartolonggeng21@gmail.com>
2022-06-04 01:22:44 +0700

    api1

--- todoapplication1/routes/api.php
+++ todoapplication1/routes/api.php
@@ -17,5 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::post('/register',[AuthController::class, 'register']);
-Route::post('/login',[AuthController::class, 'login']);
+//Route::post('/register',[AuthController::class, 'register']);
+//Route::post('/login',[AuthController::class, 'login']);
+
+$router->post('/register', 'UserController@register');
+$router->post('/login', 'AuthController@login');
+
